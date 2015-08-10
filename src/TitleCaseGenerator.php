a067fafccd4d5fac498c1498dc7f86ec1bd07856
Ashlin Aronin & Mitch Rivet <student@epicodus.com>
2015-08-10 12:24:11 -0700

    add test and spec for ignoring designated words

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -1,16 +1,19 @@
 <?php
 class TitleCaseGenerator
 {
-    public $prepositions = array(
-        'on', 'in', 'at', 'since', 'for'
-    );
+
 
 
     function makeTitleCase($input_title)
     {
+        $prepositions = array(
+            'on', 'in', 'at', 'since', 'for'
+        );
         $title_array = explode(" ", $input_title);
         $output = array();
         foreach ($title_array as $word) {
+            // initialize output word
+            $output_word = "";
 
             // Check for Mc prefix in a given word, then apply title case rules
             // Otherwise, make all letters lowercase and capitalize the first letter.
@@ -18,23 +21,30 @@ class TitleCaseGenerator
 
             $prefix_mc = strtolower(substr($word, 0, 2));
             $prefix_mac = strtolower(substr($word, 0, 3));
+            $word_lower = strtolower($word);
+
             if ($prefix_mc == "mc") {
                 $restOfName = substr($word, 2);
                 $restOfNameTitleCase = ucfirst(strtolower($restOfName));
                 $prefix_mc = ucfirst($prefix_mc);
-                array_push($output, $prefix_mc . $restOfNameTitleCase);
-            }
-            elseif ($prefix_mac == "mac") {
+                $output_word = $prefix_mc . $restOfNameTitleCase;
+            } elseif ($prefix_mac == "mac") {
                 $restOfName = substr($word, 3);
                 $restOfNameTitleCase = ucfirst(strtolower($restOfName));
                 $prefix_mac = ucfirst($prefix_mac);
-                array_push($output, $prefix_mac . $restOfNameTitleCase);
-
+                $output_word = $prefix_mac . $restOfNameTitleCase;
+            } elseif (in_array($word_lower, $prepositions)) {
+                // We already made the word lowercase so that we could test
+                // for membership in prepositions, so we don't need to do
+                // that again, we can just use the $word_lower variable.
+                $output_word = $word_lower;
+            } else {
+                $output_word = ucfirst(strtolower($word));
             }
 
-            else {
-                array_push($output, ucfirst(strtolower($word)));
-            }
+            // no matter what we checked for in between, we still need to add
+            // the changed word to the title array
+            array_push($output, $output_word);
         }
         return implode(" ", $output);
     }
