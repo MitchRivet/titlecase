2851c61d83624e9422f34f30d807de48349ae1c0
Ashlin Aronin & Mitch Rivet <student@epicodus.com>
2015-08-10 10:48:42 -0700

    routes created

--- app/app.php
+++ app/app.php
@@ -9,6 +9,15 @@
     ));
 
     // Routes here
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('form.twig');
+    });
+
+    $app->get("/view_title_case", function() use($app) {
+        $my_TitleCaseGenerator = new TitleCaseGenerator;
+        $title_cased_phrase = $my_TitleCaseGenerator->makeTitleCase($_GET['phrase']);
+        return $app['twig']->render('title_cased.twig', array('result' => $title_cased_phrase));
+    }); 
 
     return $app;
 
